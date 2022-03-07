5d64ea787a10d238680126942559f33e1e5376c8
hobit22 <hobit22@naver.com>
2022-03-07 15:38:53 +0900

    first setting

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
