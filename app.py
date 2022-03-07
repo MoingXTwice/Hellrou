45688466e1c0150d0533b6cfef90fef3a2605513
MoingXTwice <sckbs1314@gmail.com>
2022-03-07 23:35:05 +0900

    app.py conflict test

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from dotenv import load_dotenv
 import os
 import certifi
 
+from health import health_bp
+
 app = Flask(__name__)
+# app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # 환경변수 값 불러오기
 load_dotenv()
@@ -14,6 +17,8 @@ load_dotenv()
 mongo_host = os.getenv('MONGODB_HOST')
 client = MongoClient(mongo_host, tlsCAFile=certifi.where())
 db = client.hellrou
+
+app.register_blueprint(health_bp, url_prefix='/health')
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
