8ee7140e8e8c08daeab91974ef9e40edcfad0f2b
hobit22 <hobit22@naver.com>
2022-03-07 17:59:25 +0900

    hbkim teststset

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+import os
+import certifi
 
 app = Flask(__name__)
 
+# 환경변수 값 불러오기
+load_dotenv()
+
+# DB Configure
+mongo_host = os.getenv('MONGODB_HOST')
+client = MongoClient(mongo_host, tlsCAFile=certifi.where())
+db = client.hellrou
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
+    return render_template('main.html')
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
